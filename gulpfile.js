2ca605bc69e79f5320d7c1f1f46935b26df7d440
Daniel <dealenx@gmail.com>
2016-08-20 18:26:17 +0700

    1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-var gulp = require('gulp'),
-	postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var	postcss = require('gulp-postcss');
+var cssmin = require('gulp-cssmin');
 
 // Plugins
 var processors = [
-	require('autoprefixer')({ browsers: ['last 2 versions', 'Firefox > 20']  }), //https://github.com/postcss/autoprefixer
-	require('cssnext')(), //https://github.com/MoOx/postcss-cssnext
 	require('postcss-quantity-queries')(), //https://github.com/pascalduez/postcss-quantity-queries
 	require('postcss-short')(), //https://github.com/jonathantneal/postcss-short
 	require('postcss-import')(), //https://github.com/postcss/postcss-import
 	require('postcss-assets')(), //https://github.com/assetsjs/postcss-assets
 	require('postcss-short')(), //https://github.com/jonathantneal/postcss-short
 	require('postcss-nested')(), //https://github.com/postcss/postcss-nested
+	require('postcss-simple-vars')(), //https://github.com/postcss/postcss-simple-vars
+	require('autoprefixer')(), //https://github.com/postcss/autoprefixer
 ];
 
 // CSS task
@@ -18,5 +19,6 @@ gulp.task('css', function() {
 	return gulp
 		.src('app/css/global.css') //Input
 		.pipe(postcss(processors))
+		//.pipe(cssmin())
 		.pipe(gulp.dest('dist')); // Output
 });
