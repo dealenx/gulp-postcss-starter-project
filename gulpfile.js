2a5b3dc5be4712c6f4af1c23fc87caaf4bcd9a67
Daniel <dealenx@gmail.com>
2016-08-20 15:23:01 +0700

    1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,14 @@ var processors = [
 	require('postcss-quantity-queries')(), //https://github.com/pascalduez/postcss-quantity-queries
 	require('postcss-short')(), //https://github.com/jonathantneal/postcss-short
 	require('postcss-import')(), //https://github.com/postcss/postcss-import
+	require('postcss-assets')(), //https://github.com/assetsjs/postcss-assets
+	require('postcss-short')(), //https://github.com/assetsjs/postcss-short
 ];
 
 // CSS task
 gulp.task('css', function() {
 	return gulp
-		.src('app/global.css') //Input
+		.src('app/css/global.css') //Input
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('dist')); // Output
 });
